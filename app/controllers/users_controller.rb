cdd017254405166fcb4b929578f3bceb3026700c
Maxime <maxime@MacBook-Pro-de-Maxime-5.local>
2016-04-18 01:02:41 +0200

    Import du projet

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    redirect_to @user
+  else
+    render 'new'
+  end
+  end
+
+  private
+    def user_params
+    params.require(:user).permit(:name, :lastname, :description)
+  end
+end
