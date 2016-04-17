e8aac3eeb648206c16ec35f61438805e4eeb44c3
Maxime <maxime@MacBook-Pro-de-Maxime-5.local>
2016-04-18 01:16:10 +0200

    Import du projet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,17 +10,31 @@ class UsersController < ApplicationController
   def new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-    redirect_to @user
-  else
-    render 'new'
-  end
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
+  def update
+  @user = User.find(params[:id])
+
+  if @user.update(user_params)
+    redirect_to @user
+  else
+    render 'edit'
+  end
+end
+
   private
-    def user_params
+  def user_params
     params.require(:user).permit(:name, :lastname, :description)
   end
 end
